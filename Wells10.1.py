6364de02ea97312ab1f0e04a99299557fdf46929
Jerry Yuan <yhyuan@gmail.com>
2013-09-26 16:29:39 -0400

    Add ArcGIS 10.1 support

--- Wells10.1.py
+++ Wells10.1.py
@@ -74,7 +74,19 @@ def toSting(item):
 	#	itemStr = itemStr.replace("\r\n", "")		
 	return itemStr
 
-WellsReports = "WellsReport"
+cursor.execute("select BORE_HOLE_ID, WELL_ID, WELL_COMPLETED_DATE, RECEIVED_DATE, AUDIT_NO, TAG, CONTRACTOR, SWL, FINAL_STATUS_DESCR, USE1, USE2, MOE_COUNTY_DESCR, MOE_MUNICIPALITY_DESCR, CON, LOT, STREET, CITY, UTMZONE, EAST83, NORTH83, GEO, PLUG, HOLE, CM, CAS, SCRN, WAT, PT, PTD, DISINFECTED from VW_GMAP_HTML_04")
+rows = cursor.fetchall()
+
+f = open ("rows_contaion_enter.txt","w")
+f.write(str(len(rows)) + "\n")
+for row in rows:
+	line = "\t".join(map(toSting, row))
+	if ("\r\n" in line):
+		f.write(line)	
+f.close()
+
+
+WellsReports = "WellsReport0"
 WellsReportsLayer = arcpy.env.workspace + "\\"  + WellsReports
 arcpy.CreateFeatureclass_management(arcpy.env.workspace, WellsReports, "POINT", "", "DISABLED", "DISABLED", "", "", "0", "0", "0")
 arcpy.DefineProjection_management(WellsReportsLayer, "GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]")
@@ -92,16 +104,6 @@ for field in fields:
 arcpy.AddField_management(WellsReportsLayer, "PTD", "TEXT", "", "", "500", "", "NULLABLE", "NON_REQUIRED", "")
 arcpy.AddField_management(WellsReportsLayer, "DISINFECTED", "TEXT", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
 '''	
-f = open ("rows_contaion_enter.txt","w")
-cursor.execute("select BORE_HOLE_ID, WELL_ID, WELL_COMPLETED_DATE, RECEIVED_DATE, AUDIT_NO, TAG, CONTRACTOR, SWL, FINAL_STATUS_DESCR, USE1, USE2, MOE_COUNTY_DESCR, MOE_MUNICIPALITY_DESCR, CON, LOT, STREET, CITY, UTMZONE, EAST83, NORTH83, GEO, PLUG, HOLE, CM, CAS, SCRN, WAT, PT, PTD, DISINFECTED from VW_GMAP_HTML_04")
-rows = cursor.fetchall()
-
-f.write(str(len(rows)) + "\n")
-for row in rows:
-	line = "\t".join(map(toSting, row))
-	if ("\r\n" in line):
-		f.write(line)	
-f.close()
 
 cntr = 0
 try:
@@ -120,22 +122,22 @@ except Exception as e:
 	print e.message
 
 if (cntr == len(rows)):
-	print "WellsReport is created successfully!"
+	print "WellsReport0 is created successfully!"
 else:
-	print "WellsReport is NOT created successfully!"
+	print "WellsReport0 is NOT created successfully!"
 
 
-WellsReports = "WellsReport1"
-WellsReportsLayer = arcpy.env.workspace + "\\"  + WellsReports
-arcpy.CreateFeatureclass_management(arcpy.env.workspace, WellsReports, "POINT", "", "DISABLED", "DISABLED", "", "", "0", "0", "0")
-arcpy.DefineProjection_management(WellsReportsLayer, "GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]")
-arcpy.AddField_management(WellsReportsLayer, "BORE_HOLE_ID", "LONG", "", "", "", "", "NON_NULLABLE", "REQUIRED", "")
+WellsReport1 = "WellsReport1"
+#WellsReportsLayer = arcpy.env.workspace + "\\"  + WellsReport1
+arcpy.CreateFeatureclass_management(arcpy.env.workspace, WellsReport1, "POINT", "", "DISABLED", "DISABLED", "", "", "0", "0", "0")
+arcpy.DefineProjection_management(arcpy.env.workspace + "\\"  + WellsReport1, "GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]")
+arcpy.AddField_management(arcpy.env.workspace + "\\"  + WellsReport1, "BORE_HOLE_ID", "LONG", "", "", "", "", "NON_NULLABLE", "REQUIRED", "")
 fields = ["MOE_COUNTY_DESCR", "MOE_MUNICIPALITY_DESCR", "CON", "LOT", "STREET", "CITY", "UTMZONE", "EAST83", "NORTH83"]
 for field in fields:
-	arcpy.AddField_management(WellsReportsLayer, field, "TEXT", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
+	arcpy.AddField_management(arcpy.env.workspace + "\\"  + WellsReport1, field, "TEXT", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
 cntr = 0
 try:
-	with arcpy.da.InsertCursor(WellsReports, ("SHAPE@XY", "BORE_HOLE_ID", "MOE_COUNTY_DESCR", "MOE_MUNICIPALITY_DESCR", "CON", "LOT", "STREET", "CITY", "UTMZONE", "EAST83", "NORTH83")) as cur:
+	with arcpy.da.InsertCursor(WellsReport1, ("SHAPE@XY", "BORE_HOLE_ID", "MOE_COUNTY_DESCR", "MOE_MUNICIPALITY_DESCR", "CON", "LOT", "STREET", "CITY", "UTMZONE", "EAST83", "NORTH83")) as cur:
 		for row in rows:
 			row = map(toSting, row)			
 			rowValue = [(0, 0), row[0], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19]]
@@ -149,23 +151,22 @@ if (cntr == len(rows)):
 else:
 	print "WellsReport1 is NOT created successfully!"
 	
+WellsReport2 = "WellsReport2"
+#WellsReportsLayer = arcpy.env.workspace + "\\"  + WellsReport2
+arcpy.CreateFeatureclass_management(arcpy.env.workspace, WellsReport2, "POINT", "", "DISABLED", "DISABLED", "", "", "0", "0", "0")
+arcpy.DefineProjection_management(arcpy.env.workspace + "\\"  + WellsReport2, "GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]")
+arcpy.AddField_management(arcpy.env.workspace + "\\"  + WellsReport2, "BORE_HOLE_ID", "LONG", "", "", "", "", "NON_NULLABLE", "REQUIRED", "")
 
-WellsReports = "WellsReport2"
-WellsReportsLayer = arcpy.env.workspace + "\\"  + WellsReports
-arcpy.CreateFeatureclass_management(arcpy.env.workspace, WellsReports, "POINT", "", "DISABLED", "DISABLED", "", "", "0", "0", "0")
-arcpy.DefineProjection_management(WellsReportsLayer, "GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]")
-arcpy.AddField_management(WellsReportsLayer, "BORE_HOLE_ID", "LONG", "", "", "", "", "NON_NULLABLE", "REQUIRED", "")
-
-arcpy.AddField_management(WellsReportsLayer, "GEO", "TEXT", "", "", "3000", "", "NULLABLE", "NON_REQUIRED", "")
-arcpy.AddField_management(WellsReportsLayer, "PLUG", "TEXT", "", "", "1000", "", "NULLABLE", "NON_REQUIRED", "")
+arcpy.AddField_management(arcpy.env.workspace + "\\"  + WellsReport2, "GEO", "TEXT", "", "", "3000", "", "NULLABLE", "NON_REQUIRED", "")
+arcpy.AddField_management(arcpy.env.workspace + "\\"  + WellsReport2, "PLUG", "TEXT", "", "", "1000", "", "NULLABLE", "NON_REQUIRED", "")
 fields = ["HOLE", "CM", "CAS", "SCRN", "WAT", "PT"]
 for field in fields:
-	arcpy.AddField_management(WellsReportsLayer, field, "TEXT", "", "", "300", "", "NULLABLE", "NON_REQUIRED", "")
-arcpy.AddField_management(WellsReportsLayer, "PTD", "TEXT", "", "", "500", "", "NULLABLE", "NON_REQUIRED", "")
-arcpy.AddField_management(WellsReportsLayer, "DISINFECTED", "TEXT", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
+	arcpy.AddField_management(arcpy.env.workspace + "\\"  + WellsReport2, field, "TEXT", "", "", "300", "", "NULLABLE", "NON_REQUIRED", "")
+arcpy.AddField_management(arcpy.env.workspace + "\\"  + WellsReport2, "PTD", "TEXT", "", "", "500", "", "NULLABLE", "NON_REQUIRED", "")
+arcpy.AddField_management(arcpy.env.workspace + "\\"  + WellsReport2, "DISINFECTED", "TEXT", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
 cntr = 0
 try:
-	with arcpy.da.InsertCursor(WellsReports, ("SHAPE@XY", "BORE_HOLE_ID", "GEO", "PLUG", "HOLE", "CM", "CAS", "SCRN", "WAT", "PT", "PTD", "DISINFECTED")) as cur:
+	with arcpy.da.InsertCursor(WellsReport2, ("SHAPE@XY", "BORE_HOLE_ID", "GEO", "PLUG", "HOLE", "CM", "CAS", "SCRN", "WAT", "PT", "PTD", "DISINFECTED")) as cur:
 		for row in rows:
 			row = map(toSting, row)			
 			rowValue = [(0, 0), row[0], row[20], row[21], row[22], row[23], row[24], row[25], row[26], row[27], row[28], row[29]]
@@ -179,12 +180,25 @@ if (cntr == len(rows)):
 else:
 	print "WellsReport2 is NOT created successfully!"
 
-arcpy.JoinField_management(arcpy.env.workspace + "\\WellsReport", "BORE_HOLE_ID", arcpy.env.workspace + "\\WellsReport1", "BORE_HOLE_ID", "")
-arcpy.JoinField_management(arcpy.env.workspace + "\\WellsReport", "BORE_HOLE_ID", arcpy.env.workspace + "\\WellsReport2", "BORE_HOLE_ID", "")
-arcpy.FeatureClassToFeatureClass_conversion(arcpy.env.workspace + "\\WellsReport", arcpy.env.workspace, "WellsReports", "", "BORE_HOLE_ID \"BORE_HOLE_ID\" true false true 4 Long 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,BORE_HOLE_ID,-1,-1," + arcpy.env.workspace + "\\WellsReport,BORE_HOLE_ID,-1,-1," + arcpy.env.workspace + "\\WellsReport,BORE_HOLE_ID,-1,-1;WELL_ID \"WELL_ID\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,WELL_ID,-1,-1;BHK \"BHK\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,BHK,-1,-1;WELL_COMPLETED_DATE \"WELL_COMPLETED_DATE\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,WELL_COMPLETED_DATE,-1,-1;RECEIVED_DATE \"RECEIVED_DATE\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,RECEIVED_DATE,-1,-1;AUDIT_NO \"AUDIT_NO\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,AUDIT_NO,-1,-1;TAG \"TAG\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,TAG,-1,-1;CONTRACTOR \"CONTRACTOR\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,CONTRACTOR,-1,-1;SWL \"SWL\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,SWL,-1,-1;FINAL_STATUS_DESCR \"FINAL_STATUS_DESCR\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,FINAL_STATUS_DESCR,-1,-1;USE1 \"USE1\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,USE1,-1,-1;USE2 \"USE2\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,USE2,-1,-1;MOE_COUNTY_DESCR \"MOE_COUNTY_DESCR\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,MOE_COUNTY_DESCR,-1,-1;MOE_MUNICIPALITY_DESCR \"MOE_MUNICIPALITY_DESCR\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,MOE_MUNICIPALITY_DESCR,-1,-1;CON \"CON\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,CON,-1,-1;LOT \"LOT\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,LOT,-1,-1;STREET \"STREET\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,STREET,-1,-1;CITY \"CITY\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,CITY,-1,-1;UTMZONE \"UTMZONE\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,UTMZONE,-1,-1;EAST83 \"EAST83\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,EAST83,-1,-1;NORTH83 \"NORTH83\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,NORTH83,-1,-1;GEO \"GEO\" true true false 3000 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,GEO,-1,-1;PLUG \"PLUG\" true true false 1000 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,PLUG,-1,-1;HOLE \"HOLE\" true true false 300 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,HOLE,-1,-1;CM \"CM\" true true false 300 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,CM,-1,-1;CAS \"CAS\" true true false 300 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,CAS,-1,-1;SCRN \"SCRN\" true true false 300 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,SCRN,-1,-1;WAT \"WAT\" true true false 300 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,WAT,-1,-1;PT \"PT\" true true false 300 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,PT,-1,-1;PTD \"PTD\" true true false 500 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,PTD,-1,-1;DISINFECTED \"DISINFECTED\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport,DISINFECTED,-1,-1", "")
-arcpy.Delete_management( arcpy.env.workspace + "\\WellsReport" , "FeatureClass")
+arcpy.MakeFeatureLayer_management (arcpy.env.workspace + "\\WellsReport0",  "WellsReportJoin")
+arcpy.AddJoin_management("WellsReportJoin", "BORE_HOLE_ID", arcpy.env.workspace + "\\WellsReport1", "BORE_HOLE_ID")
+arcpy.AddJoin_management("WellsReportJoin", "BORE_HOLE_ID", arcpy.env.workspace + "\\WellsReport2", "BORE_HOLE_ID")
+arcpy.CopyFeatures_management("WellsReportJoin", arcpy.env.workspace + "\\WellsReports")
+print "WellsReports is created successfully!"
+
+#arcpy.JoinField_management(arcpy.env.workspace + "\\WellsReport0", "BORE_HOLE_ID", arcpy.env.workspace + "\\WellsReport1", "BORE_HOLE_ID", "")
+#arcpy.FeatureClassToFeatureClass_conversion(arcpy.env.workspace + "\\WellsReport0", arcpy.env.workspace, "WellsReport3", "", "BORE_HOLE_ID \"BORE_HOLE_ID\" true false true 4 Long 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,BORE_HOLE_ID,-1,-1," + arcpy.env.workspace + "\\WellsReport0,BORE_HOLE_ID,-1,-1;WELL_ID \"WELL_ID\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,WELL_ID,-1,-1;BHK \"BHK\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,BHK,-1,-1;WELL_COMPLETED_DATE \"WELL_COMPLETED_DATE\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,WELL_COMPLETED_DATE,-1,-1;RECEIVED_DATE \"RECEIVED_DATE\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,RECEIVED_DATE,-1,-1;AUDIT_NO \"AUDIT_NO\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,AUDIT_NO,-1,-1;TAG \"TAG\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,TAG,-1,-1;CONTRACTOR \"CONTRACTOR\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,CONTRACTOR,-1,-1;SWL \"SWL\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,SWL,-1,-1;FINAL_STATUS_DESCR \"FINAL_STATUS_DESCR\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,FINAL_STATUS_DESCR,-1,-1;USE1 \"USE1\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,USE1,-1,-1;USE2 \"USE2\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,USE2,-1,-1;MOE_COUNTY_DESCR \"MOE_COUNTY_DESCR\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,MOE_COUNTY_DESCR,-1,-1;MOE_MUNICIPALITY_DESCR \"MOE_MUNICIPALITY_DESCR\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,MOE_MUNICIPALITY_DESCR,-1,-1;CON \"CON\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,CON,-1,-1;LOT \"LOT\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,LOT,-1,-1;STREET \"STREET\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,STREET,-1,-1;CITY \"CITY\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,CITY,-1,-1;UTMZONE \"UTMZONE\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,UTMZONE,-1,-1;EAST83 \"EAST83\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,EAST83,-1,-1;NORTH83 \"NORTH83\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport0,NORTH83,-1,-1", "")
+#arcpy.JoinField_management(arcpy.env.workspace + "\\WellsReport3", "BORE_HOLE_ID", arcpy.env.workspace + "\\WellsReport2", "BORE_HOLE_ID", "")
+#arcpy.FeatureClassToFeatureClass_conversion(arcpy.env.workspace + "\\WellsReport3", arcpy.env.workspace, "WellsReports", "", "BORE_HOLE_ID \"BORE_HOLE_ID\" true false false 4 Long 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,BORE_HOLE_ID,-1,-1," + arcpy.env.workspace + "\\WellsReport3,BORE_HOLE_ID,-1,-1;WELL_ID \"WELL_ID\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,WELL_ID,-1,-1;BHK \"BHK\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,BHK,-1,-1;WELL_COMPLETED_DATE \"WELL_COMPLETED_DATE\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,WELL_COMPLETED_DATE,-1,-1;RECEIVED_DATE \"RECEIVED_DATE\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,RECEIVED_DATE,-1,-1;AUDIT_NO \"AUDIT_NO\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,AUDIT_NO,-1,-1;TAG \"TAG\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,TAG,-1,-1;CONTRACTOR \"CONTRACTOR\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,CONTRACTOR,-1,-1;SWL \"SWL\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,SWL,-1,-1;FINAL_STATUS_DESCR \"FINAL_STATUS_DESCR\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,FINAL_STATUS_DESCR,-1,-1;USE1 \"USE1\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,USE1,-1,-1;USE2 \"USE2\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,USE2,-1,-1;MOE_COUNTY_DESCR \"MOE_COUNTY_DESCR\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,MOE_COUNTY_DESCR,-1,-1;MOE_MUNICIPALITY_DESCR \"MOE_MUNICIPALITY_DESCR\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,MOE_MUNICIPALITY_DESCR,-1,-1;CON \"CON\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,CON,-1,-1;LOT \"LOT\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,LOT,-1,-1;STREET \"STREET\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,STREET,-1,-1;CITY \"CITY\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,CITY,-1,-1;UTMZONE \"UTMZONE\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,UTMZONE,-1,-1;EAST83 \"EAST83\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,EAST83,-1,-1;NORTH83 \"NORTH83\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,NORTH83,-1,-1;GEO \"GEO\" true true false 3000 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,GEO,-1,-1;PLUG \"PLUG\" true true false 1000 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,PLUG,-1,-1;HOLE \"HOLE\" true true false 300 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,HOLE,-1,-1;CM \"CM\" true true false 300 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,CM,-1,-1;CAS \"CAS\" true true false 300 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,CAS,-1,-1;SCRN \"SCRN\" true true false 300 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,SCRN,-1,-1;WAT \"WAT\" true true false 300 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,WAT,-1,-1;PT \"PT\" true true false 300 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,PT,-1,-1;PTD \"PTD\" true true false 500 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,PTD,-1,-1;DISINFECTED \"DISINFECTED\" true true false 255 Text 0 0 ,First,#," + arcpy.env.workspace + "\\WellsReport3,DISINFECTED,-1,-1", "")
+
+# Remove useless layers
+arcpy.Delete_management( arcpy.env.workspace + "\\WellsReport0" , "FeatureClass")
+print "WellsReport0 is removed successfully!"
 arcpy.Delete_management( arcpy.env.workspace + "\\WellsReport1" , "FeatureClass")
+print "WellsReport1 is removed successfully!"
 arcpy.Delete_management( arcpy.env.workspace + "\\WellsReport2" , "FeatureClass")
+print "WellsReport2 is removed successfully!"
+#arcpy.Delete_management( arcpy.env.workspace + "\\WellsReport3" , "FeatureClass")
 
 # Prepare the msd, mxd, and readme.txt
 os.system("copy " + INPUT_PATH + "\\Wells.msd " + OUTPUT_PATH)
